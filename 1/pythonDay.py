4d3ed94e0da232f4a9141048a41c753877046c86
Tom <95441035+swartom@users.noreply.github.com>
2022-12-01 08:29:44 +0000

    rm spacing

--- 1/pythonDay.py
+++ 1/pythonDay.py
@@ -1,6 +1,6 @@
 totals = list()
 with open("data.txt",'r') as elves:
-    for elf in elves.read().strip().split("\n\n") :
+    for elf in elves.read().strip().split("\n\n"):
         totals.append(sum(int(calories) for calories in elf.split("\n")))
     
 totals.sort()
