7c0e23bb47e4cd72c4b5637cf2fa86be48611296
= <=>
2022-12-06 12:27:22 +0000

    added to sys

--- 3/day3.py
+++ 3/day3.py
@@ -2,6 +2,6 @@ totals = ""
 with open("input.txt") as f:
     for rucksack in f.read().strip().split("\n"):
         totals += ("".join(filter(lambda a : a != None,set(map(lambda a : a if (a in rucksack[-len(rucksack)//2 :]) else None, rucksack[:len(rucksack)//2 ])))))
-
+# Got input now sum!
 sum = sum(list(map( lambda char : (ord(char)-96 if ord(char) > 96 else ord(char)-64+26),totals)))
 print(f"First Answer: {sum}")
